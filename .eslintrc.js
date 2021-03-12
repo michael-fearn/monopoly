9ee5e61d99a371a8067fc8666488be9ca2c0e522
Michael Fearn <fearn.michael@gmail.com>
2021-03-11 17:01:06 -0700

    refind eslintrc and husky test

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,9 @@ module.exports = {
     root: true,
     env: { node: true },
     parser: "@typescript-eslint/parser",
-    plugins: ["@typescript-eslint", "prettier"],
     extends: [
         "eslint:recommended",
-        "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended",
-        "prettier",
+        "plugin:prettier/recommended",
     ],
 };
